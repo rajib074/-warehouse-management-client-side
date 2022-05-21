a4ec738d7ef06411e27a3c05a9a2aba2ae43463d
rajib074 <rajib01711173241@gmail.com>
2022-05-21 23:09:40 +0600

    firebase and readme update

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,13 +2,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClDP-6xWMcV7A8PVJKT8P1Wn_B8FiNEjc",
-  authDomain: "rajib-enterprice.firebaseapp.com",
-  projectId: "rajib-enterprice",
-  storageBucket: "rajib-enterprice.appspot.com",
-  messagingSenderId: "395952466878",
-  appId: "1:395952466878:web:178b2fc0bb87cedb51e7ef",
-  measurementId: "G-K424V9KQWG"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 
